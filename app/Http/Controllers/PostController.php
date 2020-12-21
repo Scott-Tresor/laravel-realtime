3aac24a7cbdeba616fceba03577e3d7e7ffd607b
Scott Tresor <scotttresor@gmail.com>
2020-12-21 16:22:54 +0200

    feat: mise a jours du controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use  App\Events\PostCreatedEvent;
 class PostController extends Controller
 {
   public function index(){
-    $event = ['title'=>'congo mon pays!'];
+    $event = ['title'=>'congo mon pays! mise en place de communication avec broadcast'];
 
     broadcast(new PostCreatedEvent($event))->toOthers();
     return 'message send';
